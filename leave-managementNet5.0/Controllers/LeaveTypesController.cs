b1ce5278a6f5e8d26037b55a1f1622410dd56743
Muhammed Fatih Özdil <muhammedfatihozdil@gmail.com>
2021-12-08 16:52:38 +0300

    setup controller and actions

---
+++ leave-managementNet5.0/Controllers/LeaveTypesController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using leave_managementNet5._0.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_managementNet5._0.Controllers
+{
+    public class LeaveTypesController : Controller
+    {
+        private readonly ILeaveTypeRepository _repo;
+        private readonly IMapper _mapper;
+
+        public LeaveTypesController(ILeaveTypeRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;  
+
+        }
+
+        // GET: LeaveTypesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LeaveTypesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LeaveTypesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LeaveTypesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
